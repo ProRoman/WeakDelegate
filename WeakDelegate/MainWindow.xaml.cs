cd260cb09733df993fa4bc032f6e29be772a744f
ProRoman <Die-Helden5@yandex.ru>
2017-05-20 21:23:51 +0300

    create WeakDelegate class

---
+++ WeakDelegate/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeakDelegate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WeakDelegate<EventHandler> _click = new WeakDelegate<EventHandler>();
+
+        private IList<Subscriber> subscribers = new List<Subscriber>();
+
+        private int count = 1;
+
+        public event EventHandler Click
+        {
+            add { _click.Combine(value); }
+            remove { _click.Remove(value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                subscribers.Add(new Subscriber(count++));
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            subscribers.Clear();
+        }
+    }
+
+    public class Subscriber
+    {
+        private readonly String _name;
+
+        public Subscriber(int number)
+        {
+            _name = "Subscriber" + number;
+            ((MainWindow) Application.Current.MainWindow).Click += OnClick;
+        }
+
+        private void OnClick(object sender, EventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        ~Subscriber()
+        {
+            Console.WriteLine(_name + " Destroed");
+        }
+    }
+}
---
+++ WeakDelegate/WeakDelegate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WeakDelegate
+{
+    public class WeakDelegate<TDelegate> where TDelegate : class
+    {
+        private class MethodTarget
+        {
+            public readonly WeakReference Reference;
+            public readonly MethodInfo Method;
+
+            public MethodTarget(Delegate d)
+            {
+                Reference = new WeakReference(d.Target);
+                Method = d.Method;
+            }
+        }
+
+        private readonly List<MethodTarget> _targets = new List<MethodTarget>();
+
+        public WeakDelegate()
+        {
+            if (!typeof (TDelegate).IsSubclassOf(typeof (Delegate)))
+                throw new InvalidOperationException("TDelegate must bе а delegate type");
+        }
+
+        public void Combine(TDelegate target)
+        {
+            Delegate targetDelegate = target as Delegate;
+            if (targetDelegate != null)
+            {
+                var invocationList = targetDelegate.GetInvocationList();
+                foreach (Delegate d in invocationList)
+                {
+                    _targets.Add(new MethodTarget(d));
+                }
+            }
+        }
+
+        public void Remove(TDelegate target)
+        {
+            Delegate targetDelegate = target as Delegate;
+            if (targetDelegate != null)
+            {
+                var invocationList = targetDelegate.GetInvocationList();
+                foreach (MethodTarget mt in invocationList.Select(d => _targets.
+                    Find(
+                        w =>
+                            Equals(d.Target, (w.Reference != null ? Target : null)) &&
+                            Equals(d.Method.MethodHandle, w.Method.MethodHandle))).Where(mt => mt != null))
+                {
+                    _targets.Remove(mt);
+                }
+            }
+        }
+
+        public TDelegate Target
+        {
+            get
+            {
+                var deadRefs = new List<MethodTarget>();
+                Delegate combinedTarget = null;
+                foreach (MethodTarget mt in _targets.ToArray())
+                {
+                    WeakReference wr = mt.Reference;
+                    // Static delegate or object delegate
+                    if (wr == null || wr.Target != null)
+                    {
+                        var newDelegate = Delegate.CreateDelegate(typeof (TDelegate), wr != null ? wr.Target : null, mt.Method);
+                        combinedTarget = Delegate.Combine(combinedTarget, newDelegate);
+                    }
+                    else
+                    {
+                        _targets.Remove(mt);
+                    }
+                }
+                return combinedTarget as TDelegate;
+            }
+            set
+            {
+                _targets.Clear();
+                Combine(value);
+            }
+        }
+    }
+}
